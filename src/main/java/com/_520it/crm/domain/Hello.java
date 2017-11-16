791e8cd795c164f05d3f8c33126ac756a7b0d87e
xiaodongge <xiaodongge@qq.com>
2017-11-16 21:41:22 +0800

    修改hello类

--- src/main/java/com/_520it/crm/domain/Hello.java
+++ src/main/java/com/_520it/crm/domain/Hello.java
@@ -2,6 +2,6 @@ package com._520it.crm.domain;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
+        System.out.println("你才是傻瓜");
     }
 }
